94408ae0141517966bc84767b93249118ce2cb8a
mihir-madhani <mmadhan1@gmail.com>
2018-09-23 03:21:34 -0400

    doing

--- poker.c
+++ poker.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int winner[5],trial[5];
 
 void score_calc(char,char,char,char,char,char,char,char,char,char);
+void different_numbers(char,char,char,char,char,int[]);
 
 void main()
 {
@@ -28,15 +30,32 @@ void main()
     p1_number[4]=buff[24];p1_suit[4]=buff[25];p2_number[4]=buff[24];
     p2_suit[4]=buff[25];p3_number[4]=buff[24];p3_suit[4]=buff[25];
   //}
-  printf("%c\n",p1_number[4]);
+  
+  printf("%i\n",r);
   int x[5];
   score_calc(p1_number[0],p1_suit[0],p1_number[1],p1_suit[1],p1_number[2],p1_suit[2],
     p1_number[3],p1_suit[3],p1_number[4],p1_suit[4]);
   fclose(fp);
 }
 
-void score_calc(char p1_number,char p1_suit,char p2_number,char p2_suit,
-  char p3_number,char p3_suit,char p4_number,char p4_suit,char p5_number,char p6_suit)
+void score_calc(char card1n,char card1s,char card2n,char card2s,char card3n,
+  char card3s,char card4n,char card4s,char card5n,char card5s)
 {
-  ;
+  int x[5]={0,0,0,0,0};
+  //int diff_num=
+  different_numbers(card1n,card2n,card3n,card4n,card5n,x);
+}
+
+void different_numbers(char card1n,char card2n,char card3n,char card4n,char card5n,int x[5])
+{
+  char y[5]={card1n,card2n,card3n,card4n,card5n};
+  char check[5]={'0','0','0','0','0'};
+  for(int i=0;i<5;i++)
+  {
+    for(int j=0;j<5;j++)
+    {
+      //if(y[i]==check[j])
+        //check[j]++;
+    }
+  }
 }
